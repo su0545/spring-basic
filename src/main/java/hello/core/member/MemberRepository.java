5b8dc34389ef446ad8be45f10d1c40bf0aa743f9
전수진 <sjning94@naver.com>
2023-02-02 19:32:17 +0900

    스프링 핵심 원리 이해1 - 예제 만들기 회원 도메인 개발까지 완료

--- src/main/java/hello/core/member/MemberRepository.java
+++ src/main/java/hello/core/member/MemberRepository.java
@@ -4,5 +4,5 @@ public interface MemberRepository {
 
     void save(Member member); //회원 저장
 
-    Member findById(Long memberId); //아이디로 회원을 찾음
+    Member findById(Long memberId); //아이디로 회원을 찾음.
 }
